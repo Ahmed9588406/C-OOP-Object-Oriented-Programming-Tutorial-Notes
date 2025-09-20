5eb696cb42c2fe6c6e06de77fb6ad398f4912f69
Ahmed9588406 <ak9588406@gmail.com>
2025-09-20 19:20:30 +0300

    event and event_handeler added

--- BankAccountApp/Form1.cs
+++ BankAccountApp/Form1.cs
@@ -2,6 +2,7 @@ namespace BankAccountApp
 {
     public partial class Form1 : Form
     {
+        List<BankAccount> bankAccounts = new List<BankAccount>();
         public Form1()
         {
             InitializeComponent();
@@ -12,27 +13,25 @@ namespace BankAccountApp
             //bankaccount.Balance = 250;
 
 
-            BankAccount bankaccount2 = new BankAccount("Mohamed Alaa");
+            //BankAccount bankaccount2 = new BankAccount("Mohamed Alaa");
             //bankaccount2.Owner = "Mohamed Alaa";
             //bankaccount2.AccountNumber = Guid.NewGuid();
             //bankaccount2.Balance = 9999;
 
 
-            BankAccount bankaccount3 = new BankAccount("Elon Mask");
+            //BankAccount bankaccount3 = new BankAccount("Elon Mask");
             //bankaccount3.Owner = "Elon Mask";
             //bankaccount3.AccountNumber = Guid.NewGuid();
             //bankaccount3.Balance = 5000;
 
-            BankAccount bankaccount4 = new BankAccount("Mark joe");
+            //BankAccount bankaccount4 = new BankAccount("Mark joe");
 
-            List<BankAccount> bankAccounts = new List<BankAccount>();
-            bankAccounts.Add(bankaccount);
-            bankAccounts.Add(bankaccount2);
-            bankAccounts.Add(bankaccount3);
-            bankAccounts.Add(bankaccount4);
+            //bankAccounts.Add(bankaccount);
+            //bankAccounts.Add(bankaccount2);
+            //bankAccounts.Add(bankaccount3);
+            //bankAccounts.Add(bankaccount4);
 
 
-            BankAccountsGrid.DataSource = bankAccounts;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -52,12 +51,32 @@ namespace BankAccountApp
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            if (string.IsNullOrWhiteSpace(OwnerName.Text))
+            {
+                MessageBox.Show("Owner name is required");
+                return;
+            }
+            string name = OwnerName.Text.Trim();
+            MessageBox.Show($"Name of the Account: {name}");
+            BankAccount newAccount = new BankAccount(name);
+            bankAccounts.Add(newAccount);
+            RefreshGrid();
+            OwnerName.Text = string.Empty;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void OwnerName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void RefreshGrid()
+        {
+            BankAccountsGrid.DataSource = null;
+            BankAccountsGrid.DataSource = bankAccounts;
         }
     }
 }
